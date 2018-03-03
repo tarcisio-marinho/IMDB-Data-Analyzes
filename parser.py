17a69af29bfd30f9403047f523a49868c10740da
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-03-03 16:11:42 -0300

    update

--- parser.py
+++ parser.py
@@ -1,10 +1,28 @@
 import pandas as pd
+import json
+
 
 
 if __name__ == "__main__":
-    dataset_credits = pd.read_csv('tmdb_5000_credits.csv')
-    dataset = pd.read_csv('tmdb_5000_movies.csv')
+    #dataset_credits = pd.read_csv('tmdb_5000_credits.csv')
+    #dataset_credits.drop(['movie_id'], 1, inplace=True)
     
-    dataset_credits.drop(['movie_id'], 1, inplace=True)
-    for f in dataset["original_title"]:
-        print(f)
+    dataset = pd.read_csv('tmdb_5000_movies.csv')
+    dataset.describe()
+    genres = dataset["genres"]
+    homepage = dataset["homepage"]
+    keywords = dataset["keywords"]
+    original_language = dataset["original_language"]
+    title = dataset["original_title"]
+    overview = dataset["overview"]
+    popularity = dataset["popularity"]
+    production_companies = dataset["produtction_companies"]
+    production_countries = dataset["production_countries"]
+    release_date = dataset["release_date"]
+    revenue = dataset["revenue"]
+    runtime = dataset["runtime"]
+    spoken_languages = dataset["spoken_languages"]
+    tagline = dataset["tagline"]
+    vote_average = dataset["vote_average"]
+    vote_count = dataset["vote_count"]
+
